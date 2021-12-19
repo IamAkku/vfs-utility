86dd96968302a829628a74752a11569bfe60b072
Akram <akramhussain135@gmail.com>
2021-12-19 16:17:29 +0530

    test

--- src/main/java/com/perfios/vfsutility/VfsUtilityApplication.java
+++ src/main/java/com/perfios/vfsutility/VfsUtilityApplication.java
@@ -19,7 +19,7 @@ VfsUtilityService vfsUtilityService;
 	public static void main(String[] args) {
 		 SpringApplication.run(VfsUtilityApplication.class, args);
 
-		System.out.println("This is first spring");
+
 	}
 
 	@Override
